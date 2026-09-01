cd3b90f5e2a2eb86a82a439e605a061f3606a295
meccamove <qq899262@naver.com>
2026-09-02 02:14:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,4 @@
-const CACHE='mecca-move-v12';
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => {
-  event.waitUntil((async()=>{
-    const keys=await caches.keys();
-    await Promise.all(keys.map(k=>caches.delete(k)));
-    await self.clients.claim();
-  })());
-});
-self.addEventListener('fetch', event => {
-  if(event.request.method!=='GET') return;
-  event.respondWith((async()=>{
-    try{
-      return await fetch(event.request,{cache:'no-store'});
-    }catch(e){
-      const cached=await caches.match(event.request);
-      if(cached) return cached;
-      throw e;
-    }
-  })());
-});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil((async()=>{
+ const ks=await caches.keys(); await Promise.all(ks.map(k=>caches.delete(k))); await self.registration.unregister();
+})()));
